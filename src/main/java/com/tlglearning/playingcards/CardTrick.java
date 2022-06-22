8e84427564bf6aba8ef109697e7e571bb6ad31ea
Eric.Flores <eflo@amazon.com>
2022-06-22 15:28:42 -0700

    made comparator a lambda in tally

--- src/main/java/com/tlglearning/playingcards/CardTrick.java
+++ src/main/java/com/tlglearning/playingcards/CardTrick.java
@@ -82,16 +82,11 @@ public class CardTrick {
         }
 
 
-        Comparator<Card> comparator = new Comparator<>() {
-
-            @Override
-            public int compare(Card card1, Card card2) {
-                int comparison = card1.getSuit().getColor().compareTo(card2.getSuit().getColor());  //DEFAULT VALUE
-                comparison = (comparison != 0) ? comparison : card1.getSuit().compareTo(card2.getSuit());
-                comparison = (comparison != 0) ? comparison : card1.getRank().compareTo(card2.getRank());
-                return comparison;
-            }
-
+        Comparator<Card> comparator = (card1, card2) -> {
+            int comparison = card1.getSuit().getColor().compareTo(card2.getSuit().getColor());
+            comparison = (comparison != 0) ? comparison : card1.getSuit().compareTo(card2.getSuit());
+            comparison = (comparison != 0) ? comparison : card1.getRank().compareTo(card2.getRank());
+            return comparison;
         };
         Collections.sort((LinkedList<Card>) blackPile, comparator);
         Collections.sort((LinkedList<Card>) redPile, comparator);
